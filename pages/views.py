6bcaf28b8d2c809113cb11605117fb96b055ad50
Abdul Rehman Zaheer <abdulrehmanzaheer2005@gmail.com>
2024-09-23 20:15:05 +0500

    Added contactView and cartView

--- pages/views.py
+++ pages/views.py
@@ -11,3 +11,9 @@ def aboutView(request):
     message='this is an about section of our project'
 
     return HttpResponse(message)
+
+def contactView(request):
+    return HttpResponse("This is a contact view")
+
+def cartView(request):
+    return HttpResponse("This is cart view")
