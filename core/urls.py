1e418ba83f01f9ee68bf0a5f5dd78df47e2b1470
Abdul Rehman Zaheer <abdulrehmanzaheer2005@gmail.com>
2024-09-23 19:14:13 +0500

    Initial Commit

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from pages.views import homeView
+from pages.views import aboutView
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('home/', homeView),
+    path('about/', aboutView)
+]
